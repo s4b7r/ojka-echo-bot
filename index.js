186ceba6457cec0121518cec0620df66488719fd
Andrej Genschel <andrej.gen@gmx.de>
2020-06-22 01:12:25 +0200

    implement echo

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var https = require('https');
+var needle = require('needle');
 var fs = require('fs');
 require('dotenv').config();
 
 var app = express();
+app.use(express.json());
 
 /**
  * @example
  * 	GET: https://<SERVER_ADDRESS>:8443/?msg=hello%20world
  */
-app.get('/', function(req, res) {
-	console.log('get-req /');
-	if(req.query.msg) {
-		res.send(req.query.msg);
-	} else {
-		res.send('hello world');
-	}
+app.post('/', (req, res) => {
+	console.log(req.body);
+	var chat_id = req.body.message.chat.id;
+	res.sendStatus(200);
+
+	needle.post('https://api.telegram.org/bot' + process.env.BOT_TOKEN + '/sendMessage', {
+		'chat_id': chat_id,
+		'text': req.body.message.text
+	});
 });
 
 https.createServer({
